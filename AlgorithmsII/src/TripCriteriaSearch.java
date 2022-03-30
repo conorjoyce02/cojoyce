c106b32c4ec9ee86b6070b9f6ba01dce6e352c67
Conor <Conor@10.6.11.170>
2022-03-30 20:43:07 +0100

    variables for criteria search

--- AlgorithmsII/src/TripCriteriaSearch.java
+++ AlgorithmsII/src/TripCriteriaSearch.java
@@ -6,6 +6,33 @@ will need to remove all invalid times, e.g., there are times in the file that st
 clearly invalid. Maximum time allowed is 23:59:59. 
  */
 
+import java.sql.Time;
+
 public class TripCriteriaSearch {
 
+	protected class TripInfo{
+		
+		protected int trip_id;
+		protected Time arrival_time;
+		protected Time departure_time;
+		protected int stop_id;
+		protected int stop_sequence;
+		protected String stop_headsign;
+		protected int pickup_type;
+		protected int drop_off_type;
+		protected double shape_dist_traveled;
+		
+		protected TripInfo(int trip_id, Time arrival_time, Time departure_time, int stop_id, int stop_sequence,
+				String stop_headsign, int pickup_type, int drop_off_type, double shape_dist_traveled) {
+			this.trip_id = trip_id;
+			this.arrival_time = arrival_time;
+			this.departure_time = departure_time;
+			this.stop_id = stop_id;
+			this.stop_sequence = stop_sequence;
+			this.stop_headsign = stop_headsign;
+			this.pickup_type = pickup_type;
+			this.drop_off_type = drop_off_type;
+			this.shape_dist_traveled = shape_dist_traveled;
+	}
+}
 }
