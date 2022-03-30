780ff4f9d7e57bebb6996c5bea392e7f742541b1
Conor <Conor@10.6.11.170>
2022-03-30 20:15:40 +0100

    initial plan for user interface

--- AlgorithmsII/src/FrontInterface.java
+++ AlgorithmsII/src/FrontInterface.java
@@ -8,4 +8,17 @@ numbers), no route possible etc.
  */
 public class FrontInterface {
 
+	System.out.println("Welcome to the Vancouver Bus System. Please choose an option to proceed.");
+	//make this into a joption pane
+	System.out.println("Enter A if you would like for the system to search for the shortest path between 2 bus stops");
+	System.out.println("Enter B if you would like for the system to search for a specific bus stop");
+	System.out.println("Enter C if you would like for the system to search all trips in a given arrival time (given certain criteria)");
+	System.out.println("Enter D if you do not want to continue");
+	
+	//read in input
+	//loop based on initial input
+	//different searches/objects to be built based on which option they care about
+	
+	//if(userinput = A)....
+	
 }
