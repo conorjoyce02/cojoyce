5f3591056c34d7ad571099de04f123f7ab73f49b
Conor <Conor@10.6.11.170>
2022-03-31 12:39:28 +0100

    Setting up user interface

--- AlgorithmsII/src/FrontInterface.java
+++ AlgorithmsII/src/FrontInterface.java
@@ -6,6 +6,10 @@ You are required to provide error checking and show appropriate messages in the 
 inputs – eg bus stop doesn’t exist, wrong format for time for bus stop (eg letters instead of
 numbers), no route possible etc. 
  */
+
+import javax.swing.*;  //joption
+import java.util.Scanner;
+
 public class FrontInterface {
 
 	public static void main (String[] args)
@@ -20,8 +24,56 @@ public class FrontInterface {
 		//read in input
 		//loop based on initial input
 		//different searches/objects to be built based on which option they care about
-		
 		//if(userinput = A)....
+		
+		//Create an object for the shortestpath - adjacency matrix
+		//ShortestGraph = graph = new ShortestGraph("stop.times.txt","transfers.txt");
+		
+		//makes arraylist for tripcriteriasearch - main of this
+		//CriteriaSearch stops = new UserInterface().new stops("stop_times.txt");
+		
+		//constructs TST for stop name
+		//TernarySearch TST = new TernarySearch("stops.txt");
+		
+		Scanner input = new Scanner (System.in);
+		
+		boolean done = false;
+		
+		while(!done)
+		{
+			if (input.hasNextLine())
+			{
+				if(input.hasNext("A")||input.hasNext("a"))
+				{
+					System.out.println("Shortest Distance from x to y");
+					done = true;
+				}
+				if(input.hasNext("B")||input.hasNext("b"))
+				{
+					System.out.println("bustop # z");
+					done = true;
+				}
+				if(input.hasNext("C")||input.hasNext("c"))
+				{
+					System.out.println("Trips that meet criteria is :");
+					done = true;
+				}
+				if(input.hasNext("D")||input.hasNext("d"))
+				{
+					System.out.println("Goodbye.");
+					done = true;
+				}
+			}
+			else
+			{
+				System.out.println("Please enter A, B, C or D");
+				input.nextLine();
+			}
+		input.close();
+		}
+		
+		
+		
 		
 	}
 	
